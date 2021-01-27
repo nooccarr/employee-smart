8656e8da028a12d358134eaa160b94eda1c9ae2b
Soohyun Lim <d2axasap@gmail.com>
2021-01-27 16:00:25 -0500

    Integrated search by name functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import Search from './components/search.jsx';
+import employee_data from './sample.js';
 
 const App = () => {
   const [employees, setEmployees] = useState([]);
+  // useEffect(() => {}, []);
+
+  const getEmployeeByName = (employeeName) => {
+    // make axios call to DB
+    let employees = [];
+    for (let i = 0; i < employee_data.length; i++) {
+      employeeName = employeeName.toLowerCase();
+      let currentEmployee = employee_data[i].name.toLowerCase().slice(0, employeeName.length);
+      if (currentEmployee === employeeName) {
+        employees.push(employee_data[i]);
+      }
+    }
+    setEmployees(employees);
+  };
 
   return (
     <div>
       <h1>employee smart</h1>
-      <Search />
+      {console.log(employees)}
+      <Search
+        getEmployeeByName={getEmployeeByName}
+      />
 
 
     </div>
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Search = (props) => {
+const Search = ({ getEmployeeByName }) => {
+  const [text, setText] = useState('');
+
+  const handleClickButton = (e) => {
+    e.preventDefault();
+    getEmployeeByName(text);
+  };
+
   return (
-    <div>Search</div>
+    <div>
+      <form>
+        {console.log(text)}
+        <input
+          type='text'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button
+          onClick={(e) => handleClickButton(e)}
+        >
+          search
+        </button>
+      </form>
+    </div>
   );
 };
 
