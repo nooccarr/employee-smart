e30dfb6522f40858098f574a4da2bbfbac3b9dbd
Soohyun Lim <d2axasap@gmail.com>
2021-01-27 14:27:32 -0500

    Created sample.js to store and access employee data in the client side. Constructed Search component for implementing searching functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Search from './components/search.jsx';
 
 const App = () => {
+  const [employees, setEmployees] = useState([]);
+
   return (
-    <div>This is App!</div>
+    <div>
+      <h1>employee smart</h1>
+      <Search />
+
+
+    </div>
   );
 };
 
