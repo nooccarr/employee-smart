a30dd2ac565a01b5c7ec4e260136400635acd1e6
Soohyun Lim <d2axasap@gmail.com>
2021-01-27 14:08:12 -0500

    Created a skeleton for the application. Configured package.json, and webpack.config.

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>This is App!</div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const SRC_DIR = path.resolve(__dirname, './src');
+const DIST_DIR = path.resolve(__dirname, './dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
