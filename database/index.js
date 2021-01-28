67a54e8b864bda88a91fe3fb1957c81659b53a69
Soohyun Lim <d2axasap@gmail.com>
2021-01-28 12:23:58 -0500

    Changed url to the psql database

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 const { config } = require('./config.js');
 
-const db = pgp(config);
+const db = pgp(`postgres://${config}@localhost:5432/employee_smart`);
 
 module.exports = db;
