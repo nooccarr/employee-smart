f043a6fab21970e298d9b5a1f46800c5a1aa746c
Soohyun Lim <d2axasap@gmail.com>
2021-01-27 22:21:19 -0500

    Created and imported schema.sql into Postgres for creation of database, and table

---
+++ database/schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE employee_smart;
+
+\c employee_smart;
+
+DROP TABLE IF EXISTS employees CASCADE;
+
+CREATE TABLE employees(
+  id serial PRIMARY KEY NOT NULL UNIQUE,
+  name varchar(50) NOT NULL,
+  age integer NOT NULL,
+  department varchar(20) NOT NULL
+);
+
+INSERT INTO employees(name, age, department)
+VALUES
+  ('Beyonce Knowles', 39, 'Music'),
+  ('Ryan Reynolds', 44, 'Film'),
+  ('Anne Hathaway', 38, 'Film'),
+  ('Ki Hong Lee', 34, 'Film'),
+  ('David Beckham', 45, 'Sports'),
+  ('Camila Cabello', 23, 'Music'),
+  ('Serena Williams', 39, 'Sports');
+
+-- psql < schema.sql
